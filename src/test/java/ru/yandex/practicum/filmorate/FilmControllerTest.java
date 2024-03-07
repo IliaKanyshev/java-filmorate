fdc7927a529a98d05197f25b49762be888c6366a
AqpP <aqpporig@gmail.com>
2024-03-08 00:37:01 +0300

    TZ-10 first try

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -3,4 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class FilmControllerTest {}
+public class FilmControllerTest {
+
+}
