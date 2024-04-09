52b653886f707d94bfc65678c1f5d3af1b08ac91
akhmed <44803512+Kumyshev@users.noreply.github.com>
2024-04-09 19:37:22 +0300

    *

--- src/main/java/ru/yandex/practicum/filmorate/service/log/LogService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/log/LogService.java
@@ -8,14 +8,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.model.EventLog;
 import ru.yandex.practicum.filmorate.storage.user.LogStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class LogService {
     private final LogStorage logStorage;
+    private final UserStorage userStorage;
 
     public List<EventLog> getLogs(Integer id) {
+        userStorage.findUserById(id);
         return logStorage.getLogs(id);
     }
 }
